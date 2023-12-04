9be6c421600697f82b0c072b3082b0891f9a071a
Domi-ni <mtr.oliveira.me@gmail.com>
2023-12-04 19:54:52 -0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -8,14 +8,19 @@ class Ball(Turtle):
         self.color("white")
         self.shapesize(0.5)
         self.penup()
-        self.x_move = 0.4
-        self.y_move = 0.4
+        self.x_move = 15
+        self.y_move = 15
 
     def move(self):
         self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
 
-    def wall_collision(self):
-        if self.ycor() >= 285 or self.ycor() <= -285:
-            self.y_move *= -1
-            self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
 
