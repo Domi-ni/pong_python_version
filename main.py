19bb7c844140d5676153006a89b7c70b0095ab36
Matheus Oliveira <112003358+Domi-ni@users.noreply.github.com>
2023-12-04 16:19:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from turtle import Screen
 from middle_line import MiddleLine
 from racket import Racket
+from ball import Ball
 
 
 my_screen = Screen()
 my_screen.bgcolor("black")
-my_screen.setup(width=600, height=600)
+my_screen.setup(width=800, height=600)
 my_screen.listen()
+my_screen.tracer(0)
 
 middle_line = MiddleLine()
 middle_line.create_line()
@@ -14,18 +16,21 @@ middle_line.create_line()
 right_racket = Racket()
 right_racket.right_racket()
 
-if right_racket.ycor() > 280:
-    pass
-else:
-    my_screen.onkeypress(key="Up", fun=right_racket.up)
-
-if right_racket.ycor() < -280:
-    pass
-else:
-    my_screen.onkeypress(key="Down", fun=right_racket.down)
-
-
 left_racket = Racket()
 left_racket.left_racket()
+
+my_screen.onkeypress(key="Up", fun=right_racket.up)
+my_screen.onkeypress(key="Down", fun=right_racket.down)
+my_screen.onkeypress(key="w", fun=left_racket.up)
+my_screen.onkeypress(key="s", fun=left_racket.down)
+
+ball = Ball()
+
+game_is_on = True
+while game_is_on:
+    my_screen.update()
+    ball.wall_collision()
+    ball.move()
+
 
 my_screen.exitonclick()
